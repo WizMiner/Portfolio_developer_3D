29d7df1a248e57af62310356aef9f3c69e060f7c
Minte <mintesinottamene0917@gmail.com>
2025-01-03 13:53:05 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Ensures the app is accessible externally
+    port: 3000, // You can choose any available port
+  },
   build: {
     // Explicitly define the output directory
     outDir: 'dist',
